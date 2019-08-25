7213721344c7b27f85414e338e40751c453825ab
alucard371 <test8q@gmail.com>
2019-08-25 23:57:27 +0200

    Send a message from HeroService

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -16,6 +16,8 @@ export class HeroService {
   ) { }
 
   getHeroes(): Observable<Hero[]> {
-    return of (HEROES);
+  // TODO: send the message _after_ fetching the heroes
+  this.messageService.add('HeroService: fetched heroes');
+  return of(HEROES);
   }
 }
