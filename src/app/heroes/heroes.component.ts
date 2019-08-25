df94d50032c3884b2772b9a5420e4a54b3635f10
alucard371 <test8q@gmail.com>
2019-08-25 20:10:32 +0200

    list heroes with *ngFor

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -8,10 +9,18 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id:1,
+  private _hero: Hero = {
+    id: 1,
     name: 'windstorm'
   };
+  public get hero(): Hero {
+    return this._hero;
+  }
+  public set hero(value: Hero) {
+    this._hero = value;
+  }
+
+  heroes = HEROES;
 
   constructor() { }
 
