0a2ecc4880694828b1da9d7bc06e3388e9f8f7d3
alucard371 <test8q@gmail.com>
2019-08-25 23:41:53 +0200

    suscribe in HeroesComponent

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -42,7 +42,8 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
 
 }
